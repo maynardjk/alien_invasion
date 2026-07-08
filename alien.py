ff8bdd5b631fa2d3fb811fcb71272a3857b4a0a4
Tomasz Przychodzki <maynrd@wp.pl>
2026-07-08 19:36:45 +0200

    The fleet moves, detects edges, changes direction, drops down

--- alien.py
+++ alien.py
@@ -24,9 +24,16 @@ class Alien(Sprite):
     def blitme(self):
         """Draw the alien at its current location"""
         self.screen.blit(self.image, self.rect)
-        
+
+    def check_edges(self):
+        """Return true if alien is at the edge of the screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <=0:
+            return True
+                
     def update(self):
-        """Move the alien to the right"""
-        self.x += (self.ai_settings.alien_speed_factor)
+        """Move the alien to the right or to the left"""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
         self.rect.x = self.x
-        
