62234a3f0a1b448276fb5cc90a1416d939cb3ecc
Tomasz Przychodzki <maynrd@wp.pl>
2026-07-22 09:58:11 +0200

    Added high score

--- game_functions.py
+++ game_functions.py
@@ -117,6 +117,7 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
+        check_high_score(stats, sb)
 
     if len(aliens) == 0:
         # Destroy existing bullets, speed up game and create new fleet
@@ -214,3 +215,10 @@ def change_fleet_direction(ai_settings, aliens):
     for alien in aliens.sprites():
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
+
+def check_high_score(stats, sb):
+    """Check to see if there is a new highscore"""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
+    
